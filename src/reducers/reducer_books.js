7420f03f02467dc5a2c66a758d665605f9c9b1b4
Noah Schutte <noahschutte@gmail.com>
2016-07-14 14:41:48 -0400

    complete course

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript', pages: 88 },
+        { title: 'Ruby', pages: 20 },
+        { title: 'Redux', pages: 33 },
+        { title: 'React', pages: 15 }
+    ];
+}
